29da3b81ccdd3bd3d0bb583b7d4e049722d6f8d2
Michael Howitz <mh@gocept.com>
2011-02-03 19:12:22 +0000

    Preparing release 1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 def read(*path_elements):
     return "\n\n" + file(os.path.join(*path_elements)).read()
 
-version = '1.0dev'
+version = '1.0'
 name = 'icemac.ab.importxls'
 
 setuptools.setup(

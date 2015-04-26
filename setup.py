0642bcf0789615292d9fb8633844a7814ba55943
Michael Howitz <mh@gocept.com>
2015-04-26 15:40:49 +0200

    Back to development: 2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 def read(*path_elements):
     return "\n\n" + file(os.path.join(*path_elements)).read()
 
-version = '2.0'
+version = '2.1.dev0'
 name = 'icemac.ab.importxls'
 
 setuptools.setup(

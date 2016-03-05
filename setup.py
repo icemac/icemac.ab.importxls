02ab7cea9310b16629cd2872603c11484364c4e6
Michael Howitz <mh@gocept.com>
2016-03-05 13:52:42 +0100

    PEP-8 + PEP-257

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2008-2014 Michael Howitz
-# See also LICENSE.txt
-
 import os.path
 import setuptools
 
+
 def read(*path_elements):
+    """Read a file."""
     return "\n\n" + file(os.path.join(*path_elements)).read()
 
 version = '2.1.dev0'
@@ -18,7 +17,7 @@ setuptools.setup(
     long_description=(
         read('README.rst') +
         read('CHANGES.rst')
-        ),
+    ),
     keywords='icemac.addressbook xls excel import',
     author='Michael Howitz',
     author_email='icemac@gmx.net',
@@ -37,18 +36,18 @@ setuptools.setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 2 :: Only',
-        ],
+    ],
     packages=setuptools.find_packages('src'),
-    package_dir = {'': 'src'},
-    namespace_packages = ['icemac', 'icemac.ab'],
+    package_dir={'': 'src'},
+    namespace_packages=['icemac', 'icemac.ab'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'icemac.ab.importer > 0.9',
         'setuptools',
         'xlrd',
-        ],
-    extras_require = dict(
+    ],
+    extras_require=dict(
         test=['zope.testing >= 3.8.0',
               ]),
-    )
+)
